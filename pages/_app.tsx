f5270d4310d67b87dcb760688e3c5a62474d8e24
victor23k <victorfdez.f@gmail.com>
2023-09-29 16:46:50 +0200

    change import and type of component for typescript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { MDXProvider } from "@mdx-js/react";
-import a from "../components/mdx/a.tsx";
+import a from "../components/mdx/a";
 import Layout from "../components/layout";
 import { ThemeProvider } from "../context/ThemeContext";
 
 const components = {
-  a: a,
+  a: a as React.FC<JSX.IntrinsicElements["a"]>,
 };
 
 function MyApp({ Component, pageProps }: AppProps) {
